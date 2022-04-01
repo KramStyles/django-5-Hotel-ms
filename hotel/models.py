f867fc37f190551f9ff30aaf8d169d0dca1c83bb
kramstyles <mark.eke@decagon.dev>
2022-04-01 20:45:20 +0100

    added models to admin

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    room_categories = (
+        ('YAC', 'AC'),
+        ('NAC', 'NON-AC'),
+        ('DEL', 'DELUXE'),
+        ('QUE', 'QUEENS'),
+        ('KIN', 'KINGS')
+    )
+    number = models.IntegerField()
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+    category = models.CharField(max_length=10, choices=room_categories)
+
+    def __str__(self):
+        return f'{self.number}, {self.category} with {self.beds} for {self.capacity} people'
