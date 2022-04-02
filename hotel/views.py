eddddcb8ab4d8ca3f4ca2009bf49e6bdc8e84432
kramstyles <mark.eke@decagon.dev>
2022-04-02 16:21:22 +0100

    added booking_list.html

--- hotel/views.py
+++ hotel/views.py
@@ -6,6 +6,12 @@ from .models import Booking, Room
 
 class RoomList(generic.ListView):
     model = Room
+    context_object_name = 'rooms'
+
+    def get_context_data(self, **kwargs):
+        context = super(RoomList, self).get_context_data()
+        context['title'] = 'all rooms'
+        return context
 
 
 class BookingList(generic.ListView):
