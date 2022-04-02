1e47f7bfa84edf187a289e0228cc4003d52a9b82
kramstyles <mark.eke@decagon.dev>
2022-04-02 15:30:54 +0100

    added views for bookings and rooms

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Booking, Room
+
+
+class RoomList(generic.ListView):
+    model = Room
+
+
+class BookingList(generic.ListView):
+    model = Booking
